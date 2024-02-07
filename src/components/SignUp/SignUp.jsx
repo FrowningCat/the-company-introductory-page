c641a779ab8dbcad1552cb96198e85b7db06343a
FrowningCat <vsavinkov60@gmail.com>
2024-02-07 13:42:33 +0300

    finished working on the styles

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,5 +1,7 @@
 import './SignUp.css';
 
+import eye from '../../assets/eye.png';
+
 export default function signUp() {
     return (
         <div className="registration">
@@ -11,25 +13,43 @@ export default function signUp() {
                     <div className="registration__dataEntry__block__title">
                         Имя
                     </div>
-                    <div className="registration__dataEntry__block__input"></div>
+                    <div className="registration__dataEntry__block__input">
+                        <input className="registration__dataEntry__block__input__text"></input>
+                    </div>
                 </div>
                 <div className="registration__dataEntry__block">
                     <div className="registration__dataEntry__block__title">
                         Электронная почта
                     </div>
-                    <div className="registration__dataEntry__block__input"></div>
+                    <div className="registration__dataEntry__block__input">
+                        <input className="registration__dataEntry__block__input__text"></input>
+                    </div>
                 </div>
                 <div className="registration__dataEntry__block">
                     <div className="registration__dataEntry__block__title">
                         Пароль
                     </div>
-                    <div className="registration__dataEntry__block__input"></div>
+                    <div className="registration__dataEntry__block__input">
+                        <input className="registration__dataEntry__block__input__text"></input>
+                        <img
+                            src={eye}
+                            alt="eye img"
+                            className="registration__dataEntry__block__input__eye"
+                        />
+                    </div>
                 </div>
                 <div className="registration__dataEntry__block">
                     <div className="registration__dataEntry__block__title">
                         Подтвердите пароль
                     </div>
-                    <div className="registration__dataEntry__block__input"></div>
+                    <div className="registration__dataEntry__block__input">
+                        <input className="registration__dataEntry__block__input__text"></input>
+                        <img
+                            src={eye}
+                            alt="eye img"
+                            className="registration__dataEntry__block__input__eye"
+                        />
+                    </div>
                 </div>
             </div>
             <div className="registration__button">
