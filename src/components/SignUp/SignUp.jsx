7848deabdaad147cfea91a59f9821a16363d4eff
FrowningCat <vsavinkov60@gmail.com>
2024-02-07 11:58:57 +0300

    add style in SignUp page

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,7 +1,42 @@
+import './SignUp.css';
+
 export default function signUp() {
     return (
-        <div>
-            <h1>Hello!</h1>
+        <div className="registration">
+            <div className="registration__dataEntry">
+                <div className="registration__dataEntry__title">
+                    Регистрация
+                </div>
+                <div className="registration__dataEntry__block">
+                    <div className="registration__dataEntry__block__title">
+                        Имя
+                    </div>
+                    <div className="registration__dataEntry__block__input"></div>
+                </div>
+                <div className="registration__dataEntry__block">
+                    <div className="registration__dataEntry__block__title">
+                        Электронная почта
+                    </div>
+                    <div className="registration__dataEntry__block__input"></div>
+                </div>
+                <div className="registration__dataEntry__block">
+                    <div className="registration__dataEntry__block__title">
+                        Пароль
+                    </div>
+                    <div className="registration__dataEntry__block__input"></div>
+                </div>
+                <div className="registration__dataEntry__block">
+                    <div className="registration__dataEntry__block__title">
+                        Подтвердите пароль
+                    </div>
+                    <div className="registration__dataEntry__block__input"></div>
+                </div>
+            </div>
+            <div className="registration__button">
+                <div className="registration__button__text">
+                    Зарегестрироваться
+                </div>
+            </div>
         </div>
     );
 }
