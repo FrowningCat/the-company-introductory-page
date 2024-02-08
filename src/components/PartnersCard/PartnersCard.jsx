562cf3d3a4941b5af625935ec001cdb32d57e82e
FrowningCat <vsavinkov60@gmail.com>
2024-02-08 16:02:51 +0300

    add content in parthersCard page

--- src/components/PartnersCard/PartnersCard.jsx
+++ src/components/PartnersCard/PartnersCard.jsx
@@ -1,5 +1,8 @@
 import './ParthersCard.css';
 
+import mail from '../../assets/mail.png';
+import telephone from '../../assets/telephone.png';
+
 export default function parthersCard() {
     return (
         <div className="parthersCard">
@@ -26,6 +29,42 @@ export default function parthersCard() {
                     </text>
                 </div>
             </header>
+            <main className="parthersCard__main">
+                <div className="parthersCard__main__textInfo">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+                    officiis tempore nemo explicabo, deleniti cum, labore
+                    temporibus natus tenetur reprehenderit, ipsum alias. Optio
+                    obcaecati itaque dolorem vitae laborum nulla doloribus
+                    possimus repellendus, placeat distinctio, deleniti odio
+                    beatae id odit rerum?
+                    <br />
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+                    officiis tempore nemo explicabo, deleniti cum, labore
+                    temporibus natus tenetur reprehenderit, ipsum alias. Optio
+                    obcaecati itaque dolorem vitae laborum nulla doloribus
+                    possimus repellendus, placeat distinctio, deleniti odio
+                    beatae id odit rerum?
+                    <br />
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+                    officiis tempore nemo explicabo, deleniti cum, labore
+                    temporibus natus tenetur reprehenderit, ipsum alias. Optio
+                    obcaecati itaque dolorem vitae laborum nulla doloribus
+                    possimus repellendus, placeat distinctio, deleniti odio
+                    beatae id odit rerum?
+                </div>
+                <div className="parthersCard__main__textContact">
+                    <div className="parthersCard__main__textContact__block">
+                        <img src={telephone} alt="telephone png"></img>
+                        +7 (954) 333-44-55
+                    </div>
+                    <div className="parthersCard__main__textContact__block">
+                        <img src={mail} alt="png"></img>
+                        sykfafkar@gmail.com
+                    </div>
+                </div>
+            </main>
         </div>
     );
 }
